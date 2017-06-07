26176f84c0300015f6f81b23f6c2e456b74eda87
Becky Trammel <10798648@my.uvu.edu>
2017-06-07 11:03:02 -0600

    In Class Assignment for Arrays

---
+++ In Class Arrays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    public GameObject[] players;
+
+    void start ()
+    {
+        players = GameObject.FindGameObjectWithTag("Player");      
+        for(int i = 0; i < player.length; i++)
+        {
+            Debug.log("Player Number" + i + "is named " + player[i].name);
+
+        }
+        }
+    }
+}
+//debug.log(where it reports to
