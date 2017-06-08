d17a946025bd82515a655ab3d92a1d12d4f15611
Becky Trammel <10798648@my.uvu.edu>
2017-06-07 18:04:02 -0600

    Added else homework

---
+++ Homework Test 2/Assets/Else.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Else : MonoBehaviour
+
+//site used for reference: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/if-else
+
+{
+    public bool mountClicked = true;
+    public int a = 5;
+    public int b = 10;
+    public string TONY = (string)Console.Read();
+
+    void Start()
+    {
+        if (mountClicked)
+        {
+            print("You are now mounted.");
+        }
+        else
+        {
+            print("You are not prepared!");
+        }
+
+        if (a < 6)
+            if (b < 11)
+            {
+                print("The top one is true, Dummy.");
+            }
+            else
+            {
+                print("The Top one Isn't true, Genious.");
+            }
+
+        if (a > 9)
+        {
+            if (a < 11)
+                print("<('_')✓");
+        }
+        else
+        {
+            print(">(o)__ /)");
+        }
+
+        if (string.IsUpper(TONY))
+        {
+            print("Tony is in upper case");
+        }
+        else
+        {
+            print("Tony is in lower case");
+        }
+
+        if (a > 9)
+        {
+            if (a > 11)
+                print("<('_')✓");
+        }
+        else
+        {
+            print(">(o)__ /)");
+        }
+        else
+        {
+            print("Ƹ̵̡Ӝ̵̨̄Ʒ");
+        }
+    }
+   }
