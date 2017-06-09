6361ddf0e1eec0bcc5bd670ab221b9cdeb815fa5
Becky Trammel <10798648@my.uvu.edu>
2017-06-08 23:55:26 -0600

    added 5 while loops

---
+++ Homework Test 2/Assets/WhileLoop.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WhileLoop : MonoBehaviour
+{
+
+
+    void Start()
+    {
+        int a = 5; //set your int
+        int b = 10;
+        while (a++ < 10) //create loop and tell it how to count from your int to another number.
+        {
+            print("Count Von Count says, " + a); //tell it to print your numbers. the a without parentheses is a counted number.
+        }
+
+        while (a-- > 2)
+        {
+            print("Ah, Ah, Ah " + a);
+        }
+
+        while (a-- >= 2)
+        {
+            print(a + "Ah, Ah, Ah");
+        }
+
+        while (a-- > b)
+        {
+            print(a + "bottles on the wall");
+        }
+
+        while (a++ > b)
+        {
+            print(a + "Bottles");
+            else{
+                print("Dummy");
+            }
+        }
+    }
+}
