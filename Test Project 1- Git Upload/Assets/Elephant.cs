aa98a04db17acae6e72fe719738b917e59ee878a
Becky Trammel <10798648@my.uvu.edu>
2017-06-12 11:34:06 -0600

    created elephant script

---
+++ Test Project 1- Git Upload/Assets/Elephant.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Elephant : MammalsWithLegs {
+
+    // Properties
+    //howmanylegs overides (4)
+    //Vegeterian overrides eats
+    //
+    public bool Vegeterian = true;
+    public int AmountEaten = 1000;
+
+    void Start()
+    {
+        if(Vegeterian && AmountEaten ==1000)
+        {
+            print("Elephants eat alot");
+        }
+
+        if(Vegeterian || AmountEaten < 2000)
+        {
+            print("Elephant has eaten a lot");
+        }
+
+        if (!Vegeterian || AmountEaten < 2000)
+        {
+            print("Elephant hasn't eaten enough");
+        }
+    }
+	
+}
