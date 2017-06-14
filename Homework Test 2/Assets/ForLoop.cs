8820257480fdb4e31b3ce9d7299cf79056cb5bae
Becky Trammel <10798648@my.uvu.edu>
2017-06-13 22:17:07 -0600

    added more examples

--- Homework Test 2/Assets/ForLoop.cs
+++ Homework Test 2/Assets/ForLoop.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class ForLoop : MonoBehaviour {
 
-	// Use this for initialization
+	
 	void Start () {
 
         for (int i = 5; i <= 10; i++)
@@ -32,5 +32,20 @@ public class ForLoop : MonoBehaviour {
             print(c);
         }
         //fixed the example above to run straight through backwards c to H.
+
+        for (int b = 12; b != 15; b++)
+        {
+            print(b + " peanuts");
+        }
+
+        for (float a = 1.5f; a > 1.00f; a--)
+        {
+            print(a + " souls");
+        }
+
+        for (float a = 1.5f; a < 1.75f; a++)
+        {
+            print(a + " Fruit Candies");
+        }
     }
 }
