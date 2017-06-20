ca1be0bc796aa82488dc6d3bb397c981a89bdbcc
Becky Trammel <10798648@my.uvu.edu>
2017-06-20 14:30:57 -0600

    added to

--- Homework Test 2/Assets/ScopeAccess.cs
+++ Homework Test 2/Assets/ScopeAccess.cs
@@ -3,8 +3,20 @@ using System.Collections;
 
 public class ScopeAccess : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public int a = 1; //public: can be accessed anywhere
+    public int b = 2;
+
+    List<string> stringlist = new List<string>() { "Red", "White", "Black", "Yellow" }; //private, can only be used in this class. Trying to run this would only give the memory location.
+    List<string> stringlist2 = new List<string>() { "Emerald", "Neon", "Bronze", "Cocoa" };
+
+    protected bool = true; //protected: can be accessed by any child classes. 
+    protected bool = false;
+
+     
+    void Start () {
+        internal char c = 'C';
+        internal char d = 'D'; //Internal: Can only be accessed within the brackets it's declared in.
+        }
+
+    
 }
