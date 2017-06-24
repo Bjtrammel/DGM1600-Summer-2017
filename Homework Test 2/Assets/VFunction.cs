8c88ee00983dc3ab0fb045360cc72a95de664e74
Becky Trammel <10798648@my.uvu.edu>
2017-06-23 21:17:31 -0600

    Created Void Statements

---
+++ Homework Test 2/Assets/VFunction.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VFunction : MonoBehaviour
+{
+
+    public void Butterfly()
+    {
+        print("I'm a beautiful butterfly!");
+    }
+
+    int a = 5;
+    int b = 10;
+    public void doMath()
+    {
+       int result = a + b;
+       print(result);
+    }
+    
+    public void TellTime()
+    {
+        print(System.DateTime.Now);
+    }
+
+    public void Example4()
+    {
+        print(this.name);
+    }
+
+    public string myString = "TONY";
+    public void Example5()
+    {
+        if (myString.Any(char.IsUpper))
+        {
+            print("Tony humble brags about his dps");
+        }
+        else
+        {
+            print("Tony does actually ahve good dps");
+        }
+    }
+    void Start()
+    {
+        Butterfly();
+        doMath();
+        TellTime ();
+        Example4();
+        Example5();
+        //Example6();
+        //Example7();
+        //Example8();
+        //Example9();
+        //Example10();
+    }
+}
