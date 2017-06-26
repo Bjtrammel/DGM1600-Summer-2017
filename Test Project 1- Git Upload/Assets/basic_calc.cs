0762f6f0768d62d8f4588bf4811d6b03da98b931
Becky Trammel <10798648@my.uvu.edu>
2017-06-26 11:32:12 -0600

    created basic addition calc
    Set up UI

---
+++ Test Project 1- Git Upload/Assets/basic_calc.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class basic_calc : MonoBehaviour {
+
+    public InputField In1;
+    public InputField In2;
+    public Text result;
+
+    public void AddClick() {
+        int num = int.Parse(In1.text) + int.Parse(In2.text);
+        result.text = num.ToString();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
