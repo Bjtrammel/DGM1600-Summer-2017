d00ff01e63a4a8a7ac58e13dba780a43929fef47
Becky Trammel <10798648@my.uvu.edu>
2017-07-05 11:44:39 -0600

    functions, changed jump

---
+++ Homework Test 2/Assets/FunProp.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FunProp : MonoBehaviour {
+
+
+	void Awake()
+    {
+        print("Awake");
+    }
+
+    void OnEnable()
+    {
+        print("Enable");
+    }
+
+    void Start()
+    {
+        print("Start");
+    }
+
+    private void OnDisable()
+    {
+        print("Disable");
+    }
+
+    private void OnTriggerEnter(Collider _col)
+    {
+        print(_col.name);
+        //_col.transform.position = Vector3.zero;
+        if(_col.name != "Enemy")
+        {
+            _col.gameObject.SetActive(false);
+        }
+        else
+        {
+            print("Adding Health");
+        }
+    }
+
+    //private void OnTriggerStay()
+    //{
+    //    Print("Staying");
+    //}
+
+    private void OnTriggerExit()
+    {
+        print("Exit");
+
+    }
+}
---
+++ Homework Test 2/Assets/FunctionAdvanced.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FunctionAdvanced : MonoBehaviour {
+
+    //   public string myName = "Tony";
+    //   public int myScore = 2;
+
+    // Use this for initialization
+    //void Start () {
+    //       UseData(myName);
+    //       UseData(myScore);
+    //}
+
+    // Update is called once per frame
+    //void UseData (string _s) {
+    //       print(_s + ", Hello.");
+    //}
+
+    //   void UseData(int _i)
+    //   {
+    //       print(_i * 10);
+    //   }
+
+    public string keyWord;
+    public int health;
+
+    private void Start()
+    {
+        health = PowerUp(10);
+        keyWord = PowerUp("OU812");
+    }
+
+    string PowerUp(string _s)
+    {
+        return _s;
+    } 
+
+        int PowerUp(int _i)
+    {
+        return _i;
+    }
+
+}
