31a5bd9377320e3645acded3bfecfd7c1f496bc2
Becky Trammel <10798648@my.uvu.edu>
2017-07-10 11:12:39 -0600

    7/10
    Reorganizaiton, created follow AI, destory script, added jump veriable.

---
+++ Test Project 1- Git Upload/Assets/Goofing Off/Destroy.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Destroy : MonoBehaviour {
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "Player") {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
