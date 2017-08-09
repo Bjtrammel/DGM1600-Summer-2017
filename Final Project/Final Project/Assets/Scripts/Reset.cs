b9b79004c746f9f27b2986222550ded280e33441
Becky Trammel <10798648@my.uvu.edu>
2017-08-08 22:39:38 -0600

    Redid assets
    Do Tomorrow: TEXTURE! FIND CANVIS SO TEXT WORKS.

--- Final Project/Final Project/Assets/Scripts/Reset.cs
+++ Final Project/Final Project/Assets/Scripts/Reset.cs
@@ -6,6 +6,9 @@ public class Reset : MonoBehaviour
     public Transform spawnPoint;
     public int points;
 
+    public string[] spawnPoints = { "spawnPoint", "spawnPoint1", "spawnPoint2", "spawnPoint3", "spawnPoint4", "spawnPoint5" };
+    
+
     public void OnCollisionEnter(Collision other)
     {
         if (other.gameObject.name == "Player")
